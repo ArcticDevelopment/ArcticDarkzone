cdd6c7cefcbd3e309bd3d3b06482289c1046d7e7
KyroKrypt <kyrokrypt@gmail.com>
2021-02-17 23:07:31 -0800

    initial commit

---
+++ src/main/java/src/dev/arcticdevelopment/arcticdarkzone/ArcticDarkzone.java
@@ -0,0 +1,43 @@
+package src.dev.arcticdevelopment.arcticdarkzone;
+
+import dev.kyro.arcticapi.ArcticAPI;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArcticDarkzone extends JavaPlugin {
+
+	public static ArcticDarkzone INSTANCE;
+
+	@Override
+	public void onEnable() {
+
+		INSTANCE = this;
+
+		loadConfig();
+
+		ArcticAPI.configInit(this, "prefix", "error-prefix");
+
+		registerCommands();
+		registerListeners();
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	private void loadConfig() {
+
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+
+	private void registerCommands() {
+
+//		getCommand("printer").setExecutor(new PrinterCommand());
+	}
+
+	private void registerListeners() {
+
+//		getServer().getPluginManager().registerEvents(new PrinterEvents(), this);
+	}
+}
