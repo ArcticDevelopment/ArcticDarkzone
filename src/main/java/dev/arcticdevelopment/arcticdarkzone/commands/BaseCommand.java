386ba76ee882eff1bb4a6c5cac95282c4327831c
OX-S <finn.kliewer1@gmail.com>
2021-02-19 16:24:43 -0500

    added help command

--- src/main/java/dev/arcticdevelopment/arcticdarkzone/commands/BaseCommand.java
+++ src/main/java/dev/arcticdevelopment/arcticdarkzone/commands/BaseCommand.java
@@ -2,6 +2,7 @@ package dev.arcticdevelopment.arcticdarkzone.commands;
 
 import dev.kyro.arcticapi.commands.ABaseCommand;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -15,6 +16,9 @@ public class BaseCommand extends ABaseCommand {
 	@Override
 	public void executeBase(CommandSender sender, List<String> args) {
 		System.out.println("test");
+
+		if (!(sender instanceof Player)) return;
+		createHelp().send((Player) sender);
 
 	}
 
